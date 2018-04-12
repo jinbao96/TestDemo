3cf4a027571b8fc3f2322eb9c4cd3e7e9f052457
Jimboo <Jimboo@Jimboo>
2018-04-12 16:47:45 +0800

    2018/4/12 16:47

--- src/com/jimboo/util/EncodeUtil.java
+++ src/com/jimboo/util/EncodeUtil.java
@@ -48,11 +48,11 @@ public class EncodeUtil {
 	    Matcher matcher = pattern.matcher(str);  
 	    char ch;  
 	    while (matcher.find()) {  
-	        //group 6728  
+	          
 	        String group = matcher.group(2);  
-	        //ch:'木' 26408  
+	         
 	        ch = (char) Integer.parseInt(group, 16);  
-	        //group1 \u6728  
+	          
 	        String group1 = matcher.group(1);  
 	        str = str.replace(group1, ch + "");  
 	    }  
